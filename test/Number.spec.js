b03e6eab42132d45ca268604698112f955bc6140
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-12-06 11:24:47 +0000

    String methods tested in own test plan, started on Number methods

---
+++ test/Number.spec.js
@@ -0,0 +1,23 @@
+// Number.spec.js
+'use strict';
+
+var	ex = require('../lib/exceptional').exceptional,
+	n = ex.Number;
+
+describe('Number methods', function () {
+	var method = 'parseFloat';
+	describe(method + '()', function () {
+		var fnTest = n['' + method];
+
+		it('should answer with number from string', function () {
+			expect(fnTest('12.34')).to.be.equal(12.34);
+		});
+
+		it('should throw exception if not a floating point number', function () {
+			expect(fnTest).withParams('exceptional').to.throw(TypeError);
+			expect(fnTest).withParams('exceptional')
+				.to.throw('parseFloatEx() string argument <exceptional> ' +
+					'must be a string representing a floating point number');
+		});
+	});
+});
